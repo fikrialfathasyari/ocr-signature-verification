aba90877c4e275a637b9f49f1e796ff3c1869e8b
Fikri Al Fath Asyari <fikrialfath12@gmail.com>
2026-04-08 00:35:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -114,8 +114,8 @@ def verify(docA, docB, sim_threshold=45):
 
 if __name__ == "__main__":
     result = verify(
-        r"DocCompare/A.pdf",
-        r"DocCompare/B.pdf"
+        r"sample_data/A.pdf",
+        r"sample_data/B.pdf"
     )
     print(json.dumps(result, indent=4))
 
